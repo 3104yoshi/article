54dd64fe2550bdcff9b2e4972006f1be6edf3bdf
3104yoshi <3104adgjmp@gmail.com>
2024-08-19 22:34:14 +0900

    enable to generate include string

--- shell/aws/s3/fetch_based_on_timestamp/fetch_based_on_timestamp.sh
+++ shell/aws/s3/fetch_based_on_timestamp/fetch_based_on_timestamp.sh
@@ -5,11 +5,16 @@ TARGET_DATE=$1
 
 # Use awk to filter the table and extract file names
 # It's unnecessary to set values with FS and OFS in BEGIN statement because they are default values. 
-awk -v target="$TARGET_DATE" '
-BEGIN { FS=" "; OFS="\n"; }
+output=$(awk -v target="$TARGET_DATE" '
+BEGIN { FS=" "; }
 {
   datetime = $1 " " $2;
   if (datetime > target && NF == 4) {
     print $4;
   }
-}'
+}' | xargs -I {} echo --include "\"{}\"")
+
+# Print the concatenated string
+output="aws s3 cp [s3location] . $output --exclude \"*\""
+
+echo $output
